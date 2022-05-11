f5d4444dc2c8303e07ae24c9fe8bcf0a88029547
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-05-11 12:42:33 +0900

    learn project process

---
+++ src/main/java/org/zerock/mapper/ex02/Ex02Mapper.java
@@ -0,0 +1,9 @@
+package org.zerock.mapper.ex02;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface Ex02Mapper {
+
+	@Select("SELECT CustomerName FROM Customers WHERE CustomerId = #{id}")
+	String selectNameById(int id);
+}
