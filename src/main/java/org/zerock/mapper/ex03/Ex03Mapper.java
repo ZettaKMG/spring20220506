8884f546536485f00d3eac01058219b93359a64f
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-05-12 15:24:45 +0900

    make board list

--- src/main/java/org/zerock/mapper/ex03/Ex03Mapper.java
+++ src/main/java/org/zerock/mapper/ex03/Ex03Mapper.java
@@ -1,8 +1,14 @@
 package org.zerock.mapper.ex03;
 
+import java.util.List;
+
+import org.zerock.domain.ex02.BoardDto;
+
 public interface Ex03Mapper {
 
 	String selectCustomerNameById(int id);
 
 	String selectEmployeeFirstNameById(int id);
+
+	List<BoardDto> selectBoard();
 }
