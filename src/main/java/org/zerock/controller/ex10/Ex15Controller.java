2183dfaae5e5ac76e0ce5873e58f4b7a3565a33c
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-05-13 10:04:34 +0900

    remove board

--- src/main/java/org/zerock/controller/ex10/Ex15Controller.java
+++ src/main/java/org/zerock/controller/ex10/Ex15Controller.java
@@ -69,4 +69,17 @@ public class Ex15Controller {
 		
 		return "redirect:/ex15/board/" + board.getId();
 	}
+	
+	@PostMapping("/board/remove")
+	public String removeBoard(int id) {
+		boolean success = service.removeBoardById(id);
+		
+		if (success) {
+			
+		} else {
+			
+		}
+		
+		return "redirect:/ex15/board/list";
+	}
 }
