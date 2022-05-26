2eb39945f7967330f9f071c98cf944f7566e207c
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-05-26 09:25:49 +0900

    example table

--- src/main/webapp/WEB-INF/sql/04ExampleTables.sql
+++ src/main/webapp/WEB-INF/sql/04ExampleTables.sql
@@ -14,3 +14,34 @@ ALTER TABLE Board MODIFY COLUMN id INT PRIMARY KEY AUTO_INCREMENT;
 ALTER TABLE Reply MODIFY COLUMN id INT PRIMARY KEY AUTO_INCREMENT;
 
 ALTER TABLE Reply ADD FOREIGN KEY (board_id) REFERENCES Board(id);
+
+SELECT * FROM Board
+WHERE title LIKE '%쓰기%';
+
+SELECT * FROM w3schools.Products;
+SELECT * FROM w3schools.Categories;
+
+CREATE TABLE Products
+SELECT * FROM w3schools.Products;
+CREATE TABLE Categories
+SELECT * FROM w3schools.Categories;
+
+SELECT * FROM Products;
+SELECT * FROM Categories;
+
+SELECT * FROM Products
+WHERE CategoryID IN (1, 2);
+
+SELECT * FROM Products
+WHERE CategoryID IN (1, 2, 4);
+
+SELECT * FROM Products
+WHERE CategoryID IN (#{i1}, #{i2}, #{i3}, ... ); -- forEach 필요
+;
+
+SELECT c.CategoryName, p.ProductName, p.Unit, p.Price 
+FROM Products p JOIN Categories c ON p.CategoryID = c.CategoryID
+WHERE c.CategoryID IN (1, 3, 4);
+
+SELECT * FROM Products
+WHERE Price > 10;
---
+++ src/main/webapp/WEB-INF/sql/05ExampleTables.sql
@@ -0,0 +1,12 @@
+USE mydb6;
+DROP TABLE Member;
+CREATE TABLE Member (
+	id VARCHAR(20) PRIMARY KEY, 
+    password VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL UNIQUE,
+    nickName VARCHAR(20) NOT NULL UNIQUE,
+    inserted DATETIME NOT NULL DEFAULT NOW()
+);
+
+DESC Member;
+SELECT * FROM Member;
